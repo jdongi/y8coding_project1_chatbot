f7ee9e7e37b8e68da5c07f072e4800ca2e4b1baa
jdongi <jdong@kellettschool.com>
2024-09-02 06:59:42 +0000

    my work

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# your chatbot code here!
+name = input('Enter your name please\n')
+print(name, "It's you!", name, "i've been waiting all my life!!!", name)
+print(name.upper(), "!", name.upper(), "!!!!!!!! ")
+if name == 'Jin':
+    print('ITS JINNYJONYYJUUUUUUUUU!U!UU!U!U!U!U!UU!!!!!!!!')
+elif name == 'RONALDO':
+    print('CRITIANO RANSODO')
+else:
+    print('Its not jinyjonyju :(')
+siblings = int(input('How many sibling do you have?????'))
+if siblings == 0:
+    print('sad life :(')
+elif siblings <= 5:
+    print('!!! thats not a lot thou')
+elif siblings
