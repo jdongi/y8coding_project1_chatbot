4163d4fc5b478c6abd813c4a696e3c48492fa85c
jdongi <jdong@kellettschool.com>
2024-09-04 04:23:38 +0000

    nothing

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ if name == 'Jin':
     print('ITS JINNYJONYYJUUUUUUUUU!U!UU!U!U!U!U!UU!!!!!!!!')
 elif name == 'RONALDO':
     print('CRITIANO RANSODO')
+elif name == 'Joe Biden':
+    print('Hello Big Mac president and you are orange')
 else:
     print('Its not jinyjonyju :(')
 siblings = int(input('How many sibling do you have?????'))
 if siblings == 0:
-    print('sad life :(')
+    print('womp womp')
 elif siblings <= 5:
     print('!!! thats not a lot thou')
-elif siblings
+elif siblings <= 10:
+    print('How?!')
+elif siblings <= 100:
+    print('HOW THAT?!')
+else:
+    print('😳')
--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("test")
+weather = input("What's the weather like today? 1 - Sunny, 2 - Rainy, 3 - Windy, 4 - Snowing\n")
+if weather == '1':
+    print("It's awfully nice out today! You should go touch some grass!")
+elif weather == '2':
+    print("Welp! You should probably do something like read a book!")
+elif weather == '3':
+    print("Well that's not helpful at all!")
+elif weather == '4':
+    print("Nice!")
+askfood = str(input("Are you eating something right now?\n"))
+if askfood == 'Yes' or askfood == 'yes':
+    food = input("Nice! What are you eating?\n")
+    print('You are making me hungry! Now I want to eat ' + food)
+elif askfood == 'No' or askfood == 'no':
+    print('Oh, I thought you were eating something yummy')
+else:
+    print("I didn't quite catch that, can you repeat again?")
